28745898aebaa0b2de10a8e8ce9c1d6cfa333714
Ilhyo Sung <sih8189@gmail.com>
2020-02-08 22:55:22 -0500

    adding database connection

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const image = require("./controllers/image");
 const db = knex({
   client: "pg",
   connection: {
-    host: "postgresql-animate-62880",
-    user: "postgres",
-    password: "ilhyo",
-    database: "smart_brain"
+    host: process.env.DATABASE_URL,
+    ssl: true    
   }
 });
 
