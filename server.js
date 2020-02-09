312d111f49e83392aeff113b68c63eb51411fb0a
Ilhyo Sung <sih8189@gmail.com>
2020-02-08 22:52:18 -0500

    updating database

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require("./controllers/image");
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
+    host: "postgresql-animate-62880",
     user: "postgres",
     password: "ilhyo",
     database: "smart_brain"
